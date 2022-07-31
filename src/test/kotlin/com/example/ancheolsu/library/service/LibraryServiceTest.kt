2ceaa7f8ff9f8c7bbed0f8a27a64f564c22ad93f
Scoobi-wisdoom <Scoobi-wisdoom@users.noreply.github.com>
2022-08-01 01:03:49 +0900

    도서 검색 서비스 테스트 추가

--- src/test/kotlin/com/example/ancheolsu/library/service/LibraryServiceTest.kt
+++ src/test/kotlin/com/example/ancheolsu/library/service/LibraryServiceTest.kt
@@ -1,20 +1,35 @@
 package com.example.ancheolsu.library.service
 
+import com.example.ancheolsu.book.entity.Book
+import com.example.ancheolsu.book.repository.BookRepository
+import com.example.ancheolsu.bookcategoryrelation.entity.BookCategoryRelation
+import com.example.ancheolsu.bookcategoryrelation.repository.BookCategoryRelationRepository
+import com.example.ancheolsu.category.entity.Category
 import com.example.ancheolsu.category.repository.CategoryRepository
 import com.example.ancheolsu.common.exception.BusinessException
 import com.example.ancheolsu.library.service.model.RegisterBookModel
 import com.example.ancheolsu.library.service.model.RegisterCategoryModel
+import com.example.ancheolsu.library.service.model.SearchBookModel
+import com.querydsl.core.types.Predicate
 import io.mockk.every
 import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.assertThrows
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 
 internal class LibraryServiceTest {
     private val categoryRepository: CategoryRepository = mockk()
+    private val bookRepository: BookRepository = mockk()
+    private val bookCategoryRelationRepository: BookCategoryRelationRepository = mockk()
+
     private val libraryService = LibraryService(
         categoryRepository = categoryRepository,
-        bookRepository = mockk(),
-        bookCategoryRelationRepository = mockk(),
+        bookRepository = bookRepository,
+        bookCategoryRelationRepository = bookCategoryRelationRepository,
     )
 
     @Test
@@ -39,5 +54,59 @@ internal class LibraryServiceTest {
         assertThrows<BusinessException> {
             libraryService.registerCategory(model)
         }
+    }
+
+    @Test
+    fun `메서드 searchBook 은 title, author 로 book 을 검색하고 book 의 categories 정보도 반환한다`() {
+        val one = "one"
+        val bookOne = Book(id = 0, author = one, title = one, borrowable = true)
+        val two = "two"
+        val bookTwo = Book(id = 1, author = two, title = two, borrowable = true)
+        val pageable = PageRequest.of(0, 10)
+
+        every { bookRepository.findAll(any<Predicate>(), any<Pageable>()) } returns PageImpl(
+            listOf(bookOne, bookTwo),
+            pageable,
+            2L,
+        )
+
+        val bookCategoryRelationOne = BookCategoryRelation(id = 0, bookId = 0, categoryId = 0)
+        val bookCategoryRelationTwo = BookCategoryRelation(id = 1, bookId = 1, categoryId = 0)
+        val bookCategoryRelationThree = BookCategoryRelation(id = 2, bookId = 1, categoryId = 1)
+        every { bookCategoryRelationRepository.findAll(any<Predicate>()) } returns mutableListOf(
+            bookCategoryRelationOne,
+            bookCategoryRelationTwo,
+            bookCategoryRelationThree,
+        )
+
+        val categoryOne = Category(id = 0, categoryName = one)
+        val categoryTwo = Category(id = 1, categoryName = two)
+        every { categoryRepository.findAll(any<Predicate>()) } returns mutableListOf(
+            categoryOne,
+            categoryTwo,
+        )
+
+        val actual =
+            libraryService.searchBook(
+                SearchBookModel(
+                    title = null,
+                    author = null,
+                    pageable = pageable
+                )
+            ).toList()
+
+        assertAll(
+            { assertTrue(actual[0].title == bookOne.title) },
+            { assertTrue(actual[0].author == bookOne.author) },
+            { assertTrue(actual[0].borrowable == bookOne.borrowable) },
+            { assertTrue(actual[0].categories.size == 1) },
+            { assertTrue(actual[0].categories == listOf(categoryOne.categoryName)) },
+
+            { assertTrue(actual[1].title == bookTwo.title) },
+            { assertTrue(actual[1].author == bookTwo.author) },
+            { assertTrue(actual[1].borrowable == bookTwo.borrowable) },
+            { assertTrue(actual[1].categories.size == 2) },
+            { assertTrue(actual[1].categories == listOf(categoryOne.categoryName, categoryTwo.categoryName)) },
+        )
     }
 }
