8b4772bc0ab26c5d9cfa7e61f6c68bd6dd61449f
Scoobi-wisdoom <Scoobi-wisdoom@users.noreply.github.com>
2022-07-31 22:34:04 +0900

    테스트 의존성 mockk 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,11 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        exclude(module = "mockito-core")
+    }
+    testImplementation("io.mockk:mockk:1.10.3")
 
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
