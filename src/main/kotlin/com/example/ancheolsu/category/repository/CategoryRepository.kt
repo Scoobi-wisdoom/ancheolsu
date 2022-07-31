b72e37d7681e704de16593514f618d03bb38609e
Scoobi-wisdoom <Scoobi-wisdoom@users.noreply.github.com>
2022-08-01 01:01:22 +0900

    레포지토리에 query dsl predicate executer 추가 extends

--- src/main/kotlin/com/example/ancheolsu/category/repository/CategoryRepository.kt
+++ src/main/kotlin/com/example/ancheolsu/category/repository/CategoryRepository.kt
@@ -3,7 +3,8 @@ package com.example.ancheolsu.category.repository
 import com.example.ancheolsu.category.entity.Category
 import com.example.ancheolsu.category.entity.CategoryId
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.querydsl.QuerydslPredicateExecutor
 
-interface CategoryRepository: JpaRepository<Category, CategoryId> {
+interface CategoryRepository: JpaRepository<Category, CategoryId>, QuerydslPredicateExecutor<Category> {
     fun findByCategoryName(categoryName: String): Category?
 }
