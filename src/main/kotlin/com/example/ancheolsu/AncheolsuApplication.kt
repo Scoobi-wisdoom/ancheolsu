4f68342975bf0dd910af38e00ed4135b832b2ea2
wisdoom <wisdoom@MacBook-Pro.local>
2022-07-31 16:08:20 +0900

    초기 세팅

---
+++ src/main/kotlin/com/example/ancheolsu/AncheolsuApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ancheolsu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AncheolsuApplication
+
+fun main(args: Array<String>) {
+    runApplication<AncheolsuApplication>(*args)
+}
