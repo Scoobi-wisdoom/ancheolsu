2981604502b7ba51826b2f4e704e7ed8768941a0
Scoobi-wisdoom <Scoobi-wisdoom@users.noreply.github.com>
2022-07-31 23:19:03 +0900

    카테고리 등록 서비스 로직 추가

--- src/main/kotlin/com/example/ancheolsu/library/service/LibraryService.kt
+++ src/main/kotlin/com/example/ancheolsu/library/service/LibraryService.kt
@@ -9,6 +9,8 @@ import com.example.ancheolsu.category.repository.CategoryRepository
 import com.example.ancheolsu.common.exception.BusinessException
 import com.example.ancheolsu.library.service.model.RegisterBookModel
 import com.example.ancheolsu.library.service.model.RegisterBookResultModel
+import com.example.ancheolsu.library.service.model.RegisterCategoryModel
+import com.example.ancheolsu.library.service.model.RegisterCategoryResultModel
 import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -21,24 +23,45 @@ class LibraryService(
 ) {
     @Transactional
     fun registerBook(model: RegisterBookModel): RegisterBookResultModel {
-        val category = findByCategoryName(model.category)
+        val category = findValidCategoryName(model.category)
         val book = bookRepository.save(model.toEntity())
         saveBookCategoryRelation(book, category)
         return RegisterBookResultModel(book.id)
     }
 
-    private fun findByCategoryName(categoryName: String): Category {
-        return categoryRepository.findByCategoryName(categoryName) ?: throw BusinessException(
+    private fun findValidCategoryName(categoryName: String): Category {
+        return findByCategoryName(categoryName) ?: throw BusinessException(
             message = "category $categoryName does not exist.",
-            httpStatus = BAD_REQUEST,
             cause = null,
+            httpStatus = BAD_REQUEST,
         )
+    }
+
+    private fun findByCategoryName(categoryName: String): Category? {
+        return categoryRepository.findByCategoryName(categoryName)
     }
 
     private fun saveBookCategoryRelation(
         book: Book,
-        category: Category
+        category: Category,
     ) {
         bookCategoryRelationRepository.save(BookCategoryRelation.from(book, category))
+    }
+
+    @Transactional
+    fun registerCategory(model: RegisterCategoryModel): RegisterCategoryResultModel {
+        validateCategoryRegister(model.categoryName)
+
+        val category = categoryRepository.save(model.toEntity())
+
+        return RegisterCategoryResultModel(category.id)
+    }
+
+    private fun validateCategoryRegister(categoryName: String) {
+        if (findByCategoryName(categoryName) != null) throw BusinessException(
+            message = "category $categoryName already exists.",
+            cause = null,
+            httpStatus = BAD_REQUEST,
+        )
     }
 }
