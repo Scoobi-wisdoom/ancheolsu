906f9d9aea6664f4ea1c68f7e72f44247ea5aaee
Scoobi-wisdoom <Scoobi-wisdoom@users.noreply.github.com>
2022-08-01 22:55:41 +0900

    open api 의존성 추가 및 api doc 생성

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.5.13"
     id("io.spring.dependency-management") version "1.0.12.RELEASE"
+    id("org.springdoc.openapi-gradle-plugin") version "1.3.4"
     val kotlinVersion = "1.6.21"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
@@ -34,6 +35,10 @@ dependencies {
     implementation("com.querydsl:querydsl-jpa")
     implementation("com.querydsl:querydsl-apt")
     kapt("com.querydsl:querydsl-apt:4.2.2:jpa")
+
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.8")
+    runtimeOnly("org.springdoc:springdoc-openapi-kotlin:1.6.8")
+    implementation("org.springdoc:springdoc-openapi-data-rest:1.6.8")
 }
 
 tasks.withType<KotlinCompile> {
@@ -51,4 +56,11 @@ allOpen {
     val packageName = "javax.persistence."
     annotation("${packageName}Entity")
     annotation("${packageName}MappedSuperclass")
+}
+
+openApi {
+    apiDocsUrl.set("http://localhost:8080/library-service/v3/api-docs.yaml")
+    outputDir.set(file("./doc"))
+    outputFileName.set("library-service.yaml")
+    waitTimeInSeconds.set(90)
 }
